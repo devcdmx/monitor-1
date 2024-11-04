6f1df8b3d9375af4e604e9674cf54fdbf52caedd
kristhianmanuel <kristhianmanuel@users.noreply.github.com>
2024-11-04 10:39:14 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,9 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+
+
+    
 });
 
 Route::post('/register', [RegisteredUserController::class, 'store']);
